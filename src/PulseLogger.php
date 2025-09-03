737d18526dc516d28fdee6d1985f211965b9f5bf
PouyaniArmin <pouyaniarmin@gmail.com>
2025-09-03 19:24:26 +0330

    fix(PulseLogger): set Class and Function to null for direct logs

--- src/PulseLogger.php
+++ src/PulseLogger.php
@@ -5,12 +5,22 @@ namespace Armin\PulseLogger;
 use Exception;
 
 class PulseLogger{
-    private static ?PulseLogger $pluseLogger=null;
+    private static ?PulseLogger $pulseLogger=null;
+    private string $format="
+    Timestamp: %s
+    Level: %s
+    Message: %s
+    File: %s
+    Class: %s
+    Line: %s
+    Function: %s
+    RequestID: %s
+    ClientIP: %s\n";
     private function __construct()
     {
         
     }
-    private function __wakeup()
+    public function __wakeup()
     {
         throw new Exception("Cannot unserialize singleton class PulseLogger.");
     }
@@ -19,9 +29,21 @@ class PulseLogger{
         throw new Exception("Cannot clone singleton class PulseLogger");
     }
     public static function getInstance(){
-        if (self::$pluseLogger!==null) {
-            return self::$pluseLogger;
+        if (self::$pulseLogger!==null) {
+            return self::$pulseLogger;
         }
-        return self::$pluseLogger=new PulseLogger();
+        return self::$pulseLogger=new PulseLogger();
+    }
+    public function log($level,$message){
+        $backTrack=debug_backtrace();
+        $caller=$backTrack[1] ?? $backTrack[0];
+        $file=$caller['file'];
+        $class=($caller['class']===__CLASS__) ? null : $caller['class'];
+        $line=$caller['line'];
+        $function=($caller['function']===__FUNCTION__)? null :$caller['function'];
+        $request_id=$_SERVER['HTTP_X_REQUEST_ID'] ?? null;
+        $clinentIp=$_SERVER['REMOTE_ADDR'] ?? null;
+        $tiem=sprintf($this->format,date('c'),$level,$message,$file,$class,$line,$function,$request_id,$clinentIp);
+        file_put_contents('log.txt',$tiem,FILE_APPEND);
     }
 }
