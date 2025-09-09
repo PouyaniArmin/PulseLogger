eabef7b456c9934a19f2785f31242b2e7fa5de27
PouyaniArmin <pouyaniarmin@gmail.com>
2025-09-09 08:56:22 +0330

    Update PulseLogger: integrate formatter and handler support

--- src/PulseLogger.php
+++ src/PulseLogger.php
@@ -2,24 +2,16 @@
 
 namespace Armin\PulseLogger;
 
+use Armin\PulseLogger\Formatter\Formatter;
+use Armin\PulseLogger\Formatter\JsonFormatter;
+use Armin\PulseLogger\Formatter\TextFormatter;
+use Armin\PulseLogger\Handler\FileHandler;
 use Exception;
 
-class PulseLogger{
-    private static ?PulseLogger $pulseLogger=null;
-    private string $format="
-    Timestamp: %s
-    Level: %s
-    Message: %s
-    File: %s
-    Class: %s
-    Line: %s
-    Function: %s
-    RequestID: %s
-    ClientIP: %s\n";
-    private function __construct()
-    {
-        
-    }
+class PulseLogger
+{
+    private static ?PulseLogger $pulseLogger = null;
+    private function __construct() {}
     public function __wakeup()
     {
         throw new Exception("Cannot unserialize singleton class PulseLogger.");
@@ -28,22 +20,30 @@ class PulseLogger{
     {
         throw new Exception("Cannot clone singleton class PulseLogger");
     }
-    public static function getInstance(){
-        if (self::$pulseLogger!==null) {
+    public static function getInstance()
+    {
+        if (self::$pulseLogger !== null) {
             return self::$pulseLogger;
         }
-        return self::$pulseLogger=new PulseLogger();
+        return self::$pulseLogger = new PulseLogger();
     }
-    public function log($level,$message){
-        $backTrack=debug_backtrace();
-        $caller=$backTrack[1] ?? $backTrack[0];
-        $file=$caller['file'];
-        $class=($caller['class']===__CLASS__) ? null : $caller['class'];
-        $line=$caller['line'];
-        $function=($caller['function']===__FUNCTION__)? null :$caller['function'];
-        $request_id=$_SERVER['HTTP_X_REQUEST_ID'] ?? null;
-        $clinentIp=$_SERVER['REMOTE_ADDR'] ?? null;
-        $tiem=sprintf($this->format,date('c'),$level,$message,$file,$class,$line,$function,$request_id,$clinentIp);
-        file_put_contents('log.txt',$tiem,FILE_APPEND);
+    public function info(string $type, string $message)
+    {
+        $this->log($type, 'info', $message);
+    }
+    public function error(string $type, string $message)
+    {
+        $this->log($type, 'Erorr', $message);
+    }
+    public function log($type, string $level, $message)
+    {
+        if ($type === 'Text') {
+            $text=new TextFormatter;
+            $text->log($level,$message);
+        }
+        if ($type==='Json') {
+           $json=new JsonFormatter;
+           $json->log($level,$message);
+        }
     }
 }
